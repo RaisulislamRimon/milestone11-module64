a3567cf9c7cccb8d8a119fff30ff20377a2cca3a
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-29 07:49:02 +0000

    create-react-app client side project init.

--- module64-2/simple-node-server/index.js
+++ module64-2/simple-node-server/index.js
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const users = [
+  { id: 1, name: "John Doe", email: "johndoe@gmail.com" },
+  { id: 2, name: "Jane Doe", email: "janedoe@gmail.com" },
+  { id: 3, name: "John Smith", email: "johnsmith@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
