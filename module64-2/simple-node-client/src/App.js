7489a35e7d6d0c19a6e7a26a95169228aa525c41
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-29 08:00:14 +0000

    showing data in the ui.

--- module64-2/simple-node-client/src/App.js
+++ module64-2/simple-node-client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -7,7 +8,16 @@ const App = () => {
       .then((response) => response.json())
       .then((data) => setUsers(data));
   }, []);
-  return <div>Users : {users.length}</div>;
+  return (
+    <div className="App">
+      <h2>Users : {users.length}</h2>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
