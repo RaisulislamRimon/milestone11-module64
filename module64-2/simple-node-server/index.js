2f3b107a6be345eec89c9927fa647101df53407d
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-29 07:53:46 +0000

    now we can show data from server by solving cors policy

--- module64-2/simple-node-server/index.js
+++ module64-2/simple-node-server/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
